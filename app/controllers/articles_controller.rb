a6165f58e728838785910a2fa2753a1139d2c250
Tayyab Arif <tayyablefty786@gmail.com>
2022-07-19 10:53:17 +0500

    Add flashes remove redudancy in code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+    before_action :set_article, only: [:edit, :show, :update, :destroy]
+
     def index
         @articles = Article.all
     end
@@ -7,7 +10,8 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
+        #@article = Article.find(params[:id])   
+        #this is define below in one place to remove redudancy
     end
 
     def create
@@ -22,11 +26,11 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article=Article.find(params[:id])
+        #@article=Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
+        #@article = Article.find(params[:id])
 
         if @article.update(params_data)
 
@@ -38,9 +42,21 @@ class ArticlesController < ApplicationController
 
     end
 
+    def destroy
+        #@article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice]= "Article was deleted successfully"
+
+        redirect_to articles_path
+    end
+
 
 
     private
+
+    def set_article 
+        @article = Article.find(params[:id])
+    end
 
     def params_data
         params.require(:article).permit(:title, :description)
